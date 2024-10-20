9bd5ac2bd021a741054c87df0d1284a2215eed7e
evilcorn128 <evilcorn128@gmail.com>
2024-10-20 05:08:08 +0000

    fetch changelog now helper util

--- src/main.js
+++ src/main.js
@@ -1,133 +1,152 @@
-import { Utils } from './utils.js';
+import { Utils } from "./utils.js";
 
 async function init() {
-    let correctPassword = await Utils.fetchPassword();
+  let correctPassword = await Utils.fetchPassword();
 
-    function handleSessionCheck() {
-        if (localStorage.getItem("auth") !== correctPassword || localStorage.getItem("vertex_sso") !== 'true') {
-            localStorage.setItem('enable-beforeunload', 'false');
-            alert('Session not authorized.');
-            window.location.href = "login.html";
-        }
+  function handleSessionCheck() {
+    if (
+      localStorage.getItem("auth") !== correctPassword ||
+      localStorage.getItem("vertex_sso") !== "true"
+    ) {
+      localStorage.setItem("enable-beforeunload", "false");
+      alert("Session not authorized.");
+      window.location.href = "login.html";
     }
+  }
 
-    document.addEventListener('mousemove', handleSessionCheck);
-    handleSessionCheck();
-        
-        let currentPage = 1;
-        let itemsPerPage = 6;
-        let currentFilter = 'All';
-        let isChangelogOpen = false;
+  document.addEventListener("mousemove", handleSessionCheck);
+  handleSessionCheck();
 
-        Array.from(document.getElementsByClassName('button-action')).forEach(button => {
-            button.addEventListener('click', () => {
-                localStorage.setItem('enable-beforeunload', 'false');
-            });
+  let currentPage = 1;
+  let itemsPerPage = 6;
+  let currentFilter = "All";
+  let isChangelogOpen = false;
+
+  Array.from(document.getElementsByClassName("button-action")).forEach(
+    (button) => {
+      button.addEventListener("click", () => {
+        localStorage.setItem("enable-beforeunload", "false");
+      });
+    }
+  );
+
+  document.getElementById("search-bar").addEventListener("input", loadResults);
+  document
+    .getElementById("changelog-btn")
+    .addEventListener("click", toggleChangelog);
+  document.querySelectorAll(".filter-btn").forEach((button) => {
+    button.addEventListener("click", function () {
+      currentFilter = this.getAttribute("data-filter");
+      currentPage = 1;
+      loadResults();
+    });
+  });
+
+  loadResults();
+
+  function loadResults() {
+    const query = document.getElementById("search-bar").value.toLowerCase();
+    fetch("/data/content.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const filteredData = data.filter((item) => {
+          let categoryMatches = false;
+
+          if (currentFilter === "Apps") {
+            categoryMatches = item.category === "Apps";
+          } else if (currentFilter === "Games") {
+            categoryMatches = item.category === "Games";
+          } else {
+            categoryMatches =
+              currentFilter === "All" || item.category === currentFilter;
+          }
+
+          return (
+            categoryMatches &&
+            (item.title.toLowerCase().includes(query) ||
+              item.description.toLowerCase().includes(query))
+          );
         });
+        displayResults(filteredData);
+      });
+  }
 
-        document.getElementById('search-bar').addEventListener('input', loadResults);
-        document.getElementById('changelog-btn').addEventListener('click', toggleChangelog);
-        document.querySelectorAll('.filter-btn').forEach(button => {
-            button.addEventListener('click', function() {
-                currentFilter = this.getAttribute('data-filter');
-                currentPage = 1;
-                loadResults();
-            });
-        });
+  function displayResults(data) {
+    const results = document.getElementById("results");
+    results.innerHTML = "";
 
-        loadResults();
+    const start = (currentPage - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
 
-        function loadResults() {
-            const query = document.getElementById('search-bar').value.toLowerCase();
-            fetch('/data/content.json')
-                .then(response => response.json())
-                .then(data => {
-                    const filteredData = data.filter(item => {
-                        let categoryMatches = false;
-
-                        if (currentFilter === 'Apps') {
-                            categoryMatches = item.category === 'Apps';
-                        } else if (currentFilter === 'Games') {
-                            categoryMatches = item.category === 'Games';
-                        } else {
-                            categoryMatches = currentFilter === 'All' || item.category === currentFilter;
-                        }
-
-                        return categoryMatches && 
-                            (item.title.toLowerCase().includes(query) || item.description.toLowerCase().includes(query));
-                    });
-                    displayResults(filteredData);
-                });
-        }
-
-        function displayResults(data) {
-            const results = document.getElementById('results');
-            results.innerHTML = '';
-
-            const start = (currentPage - 1) * itemsPerPage;
-            const end = start + itemsPerPage;
-
-            data.slice(start, end).forEach(item => {
-                const card = document.createElement('div');
-                card.classList.add('card');
-                card.innerHTML = `
+    data.slice(start, end).forEach((item) => {
+      const card = document.createElement("div");
+      card.classList.add("card");
+      card.innerHTML = `
                     <img src="assets/${item.icon}" alt="Icon" class="card-icon">
                     <h3>${item.title}</h3>
                     <p class="card-description">${item.description}</p>
                 `;
-                card.addEventListener('click', () => {
-                    window.location.href = item.url;
-                });
-                results.appendChild(card);
-            });
+      card.addEventListener("click", () => {
+        window.location.href = item.url;
+      });
+      results.appendChild(card);
+    });
 
-            displayPagination(data.length);
-        }
+    displayPagination(data.length);
+  }
 
-        function displayPagination(totalItems) {
-            const pagination = document.getElementById('pagination');
-            pagination.innerHTML = '';
-            const totalPages = Math.ceil(totalItems / itemsPerPage);
+  function displayPagination(totalItems) {
+    const pagination = document.getElementById("pagination");
+    pagination.innerHTML = "";
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
 
-            for (let i = 1; i <= totalPages; i++) {
-                const btn = document.createElement('button');
-                btn.textContent = i;
-                if (i === currentPage) {
-                    btn.style.backgroundColor = '#272727';
-                    btn.style.top = '2px';
-                    btn.style.position = 'relative';
-                }
-                btn.addEventListener('click', function() {
-                    currentPage = i;
-                    loadResults();
-                });
-                pagination.appendChild(btn);
-            }
-        }
+    for (let i = 1; i <= totalPages; i++) {
+      const btn = document.createElement("button");
+      btn.textContent = i;
+      if (i === currentPage) {
+        btn.style.backgroundColor = "#272727";
+        btn.style.top = "2px";
+        btn.style.position = "relative";
+      }
+      btn.addEventListener("click", function () {
+        currentPage = i;
+        loadResults();
+      });
+      pagination.appendChild(btn);
+    }
+  }
 
-        function toggleChangelog(event) {
-            const contextMenu = document.getElementById('context-menu');
-            if (isChangelogOpen) {
-                contextMenu.style.display = 'none';
-                isChangelogOpen = false;
+  async function toggleChangelog(event) {
+    const contextMenu = document.getElementById("context-menu");
+    if (isChangelogOpen) {
+      contextMenu.style.display = "none";
+      isChangelogOpen = false;
+    } else {
+      await Utils.fetchChangelog();
+    }
+  }
+
+  function parseChangelog(data) {
+    return data
+      .split("$")
+      .map((section) => {
+        return section
+          .split("\n")
+          .map((line) => {
+            if (line.startsWith("-")) {
+              return `<div class="context-header">${line
+                .slice(1)
+                .toUpperCase()}</div>`;
+            } else if (line.startsWith(">")) {
+              return `<div class="context-entry">${line.slice(1)}</div>`;
             } else {
-                await Utils.fetchChangelog();
+              return "";
             }
-        }
-
-        function parseChangelog(data) {
-            return data.split('$').map(section => {
-                return section.split('\n').map(line => {
-                    if (line.startsWith('-')) {
-                        return `<div class="context-header">${line.slice(1).toUpperCase()}</div>`;
-                    } else if (line.startsWith('>')) {
-                        return `<div class="context-entry">${line.slice(1)}</div>`;
-                    } else {
-                        return '';
-                    }
-                }).join('');
-            }).join('<hr style="border: none; margin: 10px 0;">');
-        }
-    };
+          })
+          .join("");
+      })
+      .join('<hr style="border: none; margin: 10px 0;">');
+  }
+}
 
 init();
