c5f826d94878476b1d005ccad9be8104e1e7d8de
evilcorn128 <evilcorn128@gmail.com>
2025-02-06 01:58:07 +0000

    chat test #1 & remove new tags on some apps

---
+++ src/chat.js
@@ -0,0 +1,29 @@
+let iframe;
+
+async function createChatFrame() {
+  try {
+    const response = await fetch("https://gljwsxkst95k.share.zrok.io/");
+    const srcdoc = await response.text();
+
+    iframe = document.createElement("iframe");
+    iframe.srcdoc = srcdoc;
+    iframe.style.position = "absolute";
+    iframe.style.top = "0";
+    iframe.style.left = "80px";
+    iframe.style.width = "calc(100% - 80px)";
+    iframe.style.height = "100vh";
+    iframe.style.border = "none";
+
+    document.body.appendChild(iframe);
+  } catch (error) {
+    console.error("Error fetching the iframe content:", error);
+  }
+}
+
+function toggleChat() {
+  if (iframe) {
+    iframe.style.display = iframe.style.display === "none" ? "block" : "none";
+  } else {
+    createChatFrame();
+  }
+}
